fa8a0418e1aa889bf52a9814f33abe5cdb73432e
dalimedmrad <dalimohamed789@gmail.com>
2021-08-17 12:07:35 +0100

    first commit

---
+++ src/components/Footer/footerWrapper.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const footerWrapper = styled.div`
+
+  .footerHead{
+    background: #F5F6FA;
+    display: flex;
+    justify-content:center;
+    padding: 30px 200px 39px 195px;
+  }
+  .footerHead img{
+      width:400px;
+    
+  }
+  .footerFoot{
+    font-size:16px;
+    display: flex;
+    justify-content:center;
+  padding:30px;
+    align-items:center;
+    flex-direction: column;
+  }
+  .footerFoot h5{
+    color: #192A56;
+    font-size:16px;
+  }
+ 
+`;
+
+export default footerWrapper;
